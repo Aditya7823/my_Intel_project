32dbbbd61c648997d656173ea4711af8a6fbff16
Aditya Ajay Jaiswal <166857045+Aditya7823@users.noreply.github.com>
2024-07-04 10:25:11 +0530

    changes done in format
    some changes donte in format

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("unati2.html")
 
 @app.route("/health")
 def health():
     return render_template("health.html")
+
+@app.route("/healthpredict")
+def healthpredict():
+    return render_template("healthpredict.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
